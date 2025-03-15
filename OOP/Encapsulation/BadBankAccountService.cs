330a58a2eceb12396e0c7b4498665355993e405d
Tomislav Prstec <toprstec@vsite.hr>
2025-03-15 11:14:27 +0100

    Encapsulation solved.

---
+++ OOP/Encapsulation/BadBankAccountService.cs
@@ -0,0 +1,30 @@
+﻿namespace OOP.Encapsulation;
+
+class BadBankAccountService
+{
+    public decimal balance { get; set; }
+}
+
+public static class BadBankAccountServiceExample
+{
+    public static void Main()
+    {
+        try
+        {
+            var bankAccount = new BadBankAccountService();
+            bankAccount.balance = 1000;
+            bankAccount.balance = -1000;
+            Console.WriteLine(bankAccount.balance);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        finally
+        {
+            Console.WriteLine("Program completed");
+        }
+    }
+}
+
