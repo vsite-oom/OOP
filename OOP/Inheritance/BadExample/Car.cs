f6055418efe9e9e570f7d331a60585430e2719ec
KriZa96 <kriza9605@gmail.com>
2025-03-15 11:35:39 +0100

    Resolved Inheritance

---
+++ OOP/Inheritance/BadExample/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace OOP.Inheritance.BadExample;
+public class Car
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public Car(string brand, string model, int year)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+
+    public void Start() => Console.WriteLine("Car started...");
+
+    public void Stop() => Console.WriteLine("Car stopped...");
+
+    public void Drive() => Console.WriteLine("Car is driving...");
+}
