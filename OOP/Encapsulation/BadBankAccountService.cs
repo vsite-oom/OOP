0329577b8d80bf319d1cf07e6ce193056e7b30ad
KriZa96 <kriza9605@gmail.com>
2025-03-15 11:14:23 +0100

    Commiting Encapsulation

---
+++ OOP/Encapsulation/BadBankAccountService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Encapsulation;
+
+public class BadBankAccountService
+{
+    public decimal balance { get; set; }
+
+}
+
+public static class BadAccountSericeExample
+{
+    public static void Main()
+    {
+        try
+        {
+            var bankAccount = new BadBankAccountService();
+            bankAccount.balance = 1000;
+            bankAccount.balance = -1000;
+            Console.WriteLine(bankAccount.balance);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Program completed");
+        }
+    }
+}
